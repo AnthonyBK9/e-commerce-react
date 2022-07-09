7d73da8dfe4424d7cd123517f43590ee557794bb
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-07-09 18:28:46 -0500

    Creacion de cerrar sesion funcional

--- src/components/Login/UserLogged.jsx
+++ src/components/Login/UserLogged.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const UserLogged = () => {
+  const navigate = useNavigate()
+  const singOut = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
   return (
     <article className="login__form">
       <div className="user-logged">
@@ -9,7 +15,7 @@ const UserLogged = () => {
       <div className="user">
         <h2>User Logged</h2>
       </div>
-      <button className="login-btn-out">Sign out</button>
+      <button className="login-btn-out" onClick={singOut}>Sign out</button>
     </article>
   )
 }
